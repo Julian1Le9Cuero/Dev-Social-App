6c501be1c55a5e811517310a2504130c2e259e07
Julian <you@example.com>
2020-06-08 14:19:22 -0500

    Ready for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const userRoute = require("./routes/api/user");
 const authRoute = require("./routes/api/auth");
@@ -19,6 +20,17 @@ app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/profile", profileRoute);
 app.use("/api/posts", postsRoute);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set the build static folder
+  app.use(express.static("client/build"));
+
+  // Serve the index.html file
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
